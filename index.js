263e2954c7eb4c95747f50afa0b486761967c486
MuhammadSaidulislam <mislam171222@bscse.uiu.ac.bd>
2023-07-12 23:51:36 +0600

    cartegory

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const jwt = require('jsonwebtoken');
 const http = require('http');
 var mysql = require('mysql');
 require("dotenv").config();
-
+const path = require('path');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'image')));
 const routes = require('./routes/routes')
 
 
@@ -31,8 +32,8 @@ db.connect(function (err) {
         console.log('db connect');
     }
 })
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log("Listening port 8000"));
+const port = process.env.PORT || 8888;
+app.listen(port, () => console.log("Listening port 8888"));
 
 app.use(function (req, res, next) {
     req.db = db;
